9edc7c9bfd4007f949b3b348dbee263de38c1fb4
Nguyen Le Vu Long <vulongvn98@gmail.com>
2021-12-15 19:24:29 +0700

    tmp file name
    Signed-off-by: Nguyen Le Vu Long <vulongvn98@gmail.com>

--- cli/tempmanager.go
+++ cli/tempmanager.go
@@ -15,7 +15,7 @@ func NewTempManager() (*TempManager, error) {
 	tm := new(TempManager)
 
 	// test if we can create temp file and write to it
-	testFile, err := ioutil.TempFile("", "testnet-api-test-")
+	testFile, err := ioutil.TempFile("", "pab-go-test-")
 	if err != nil {
 		return nil, fmt.Errorf("tempmanager: fail to create temp file: %w", err)
 	}
@@ -31,7 +31,7 @@ func NewTempManager() (*TempManager, error) {
 }
 
 func (tm *TempManager) NewFile(suffix string) *os.File {
-	file, err := ioutil.TempFile("", fmt.Sprintf("testnet-api-%s-", suffix))
+	file, err := ioutil.TempFile("", fmt.Sprintf("pab-go-%s-", suffix))
 	if err != nil {
 		// We know it will likely succeed because we test ioutil.TempFile when initialize TempManager, but we still log for auditing exceptions.
 		log.Printf("tempmanager: fail to create temp file: %v\n", err)
