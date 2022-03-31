6c043c3ea2d546a9459609bc3884534294e82bd9
Nguyen Le Vu Long <vulongvn98@gmail.com>
2022-03-31 16:23:01 +0700

    fix MinimumADA function
    Signed-off-by: Nguyen Le Vu Long <vulongvn98@gmail.com>

--- ledger/asset.go
+++ ledger/asset.go
@@ -1,6 +1,9 @@
 package ledger
 
-import "strings"
+import (
+	"errors"
+	"strings"
+)
 
 var ADA = NewAsset("", "")
 
@@ -13,9 +16,21 @@ func NewAsset(currencySymbol, tokenName string) Asset {
 	return Asset{currencySymbol, tokenName}
 }
 
+func AssetFromString(s string) (Asset, error) {
+	parts := strings.Split(s, ".")
+	if len(parts) != 2 {
+		return Asset{}, errors.New("ledger.AssetFromString: asset format must be $currencySymbol.$tokenName")
+	}
+	return NewAsset(parts[0], parts[1]), nil
+}
+
 func (a Asset) Cmp(b Asset) int {
 	if a.CurrencySymbol == b.CurrencySymbol {
 		return strings.Compare(a.TokenName, b.TokenName)
 	}
 	return strings.Compare(a.CurrencySymbol, b.CurrencySymbol)
+}
+
+func (a Asset) String() string {
+	return a.CurrencySymbol + "." + a.TokenName
 }
